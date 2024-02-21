3200101d8f1c26d60465fe1c51a1e5fda0fb2d38
Bas de Ruiter <deruiterb@gmail.com>
2024-02-21 15:03:09 +0100

    Create simulation

---
+++ script.js
@@ -0,0 +1,116 @@
+noise.seed(Math.random());
+
+const canvasWidth = 800;
+const canvasHeight = 600;
+const cellSize = 10;
+const gridWidth = canvasWidth / cellSize;
+const gridHeight = canvasHeight / cellSize;
+const colors = ['red', 'orange', 'yellow'];
+const rules = {
+    'orange': 'red',
+    'red': 'yellow',
+    'yellow': 'orange',
+};
+const noiseFactor = 20;
+
+// Create the grid
+let grid = [];
+for (let row = 0; row < gridHeight; row++) {
+    grid[row] = [];
+    for (let col = 0; col < gridWidth; col++) {
+        const noiseValue = Math.abs(noise.simplex2(row / noiseFactor, col / noiseFactor));
+        // const noiseValue = Math.random();
+        const color = colors[Math.floor(noiseValue * colors.length)];
+        grid[row][col] = {row, col, color, battles: false};
+    }
+}
+
+// Setup board canvas
+const canvas = document.getElementById('board');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+const ctx = canvas.getContext('2d');
+
+// Draw the grid
+function draw() {
+    for (let row = 0; row < gridHeight; row++) {
+        for (let col = 0; col < gridWidth; col++) {
+            const cell = grid[row][col];
+            ctx.beginPath();
+            ctx.rect(cell.col * cellSize, cell.row * cellSize, cellSize, cellSize);
+            ctx.fillStyle = cell.color;
+            ctx.fill();
+        }
+    }
+}
+
+// battle two cells
+function battle(a, b, nextGrid) {
+    if ( ! a.battles) {
+        return;
+    }
+
+    nextGrid[b.row][b.col].battles = true;
+
+    if (rules[a.color] === b.color) {
+        nextGrid[b.row][b.col].color = a.color;
+    } else {
+        nextGrid[a.row][a.col].color = b.color;
+    }
+}
+
+function next() {
+    let nextGrid = JSON.parse(JSON.stringify(grid));
+
+    // To change right, we move from right to left.
+    for (let row = 0; row < gridHeight; row++) {
+        for (let col = gridWidth-2; col >= 0; col--) {
+            const right = grid[row][col+1];
+            battle(grid[row][col], right, nextGrid);
+        }
+    }
+
+    // To change bottom, we move from bottom to top.
+    for (let col = 0; col < gridWidth; col++) {
+        for (let row = gridHeight-2; row >= 0; row--) {
+            const bottom = grid[row+1][col];
+            battle(grid[row][col], bottom, nextGrid);
+        }
+    }
+
+    // left
+    for (let row = 0; row < gridHeight; row++) {
+        for (let col = 1; col < gridWidth; col++) {
+            const left = grid[row][col-1];
+            battle(grid[row][col], left, nextGrid);
+        }
+    }
+
+    // top
+    for (let col = 0; col < gridWidth; col++) {
+        for (let row = 1; row < gridHeight; row++) {
+            const top = grid[row-1][col];
+            battle(grid[row][col], top, nextGrid);
+        }
+    }
+
+    return nextGrid;
+}
+
+// Start at the center
+let rowCenter = Math.floor(grid.length / 2);
+let colCenter = Math.floor(grid[0].length / 2);
+grid[rowCenter][colCenter].battles = true;
+draw();
+
+// run the simulation for i times.
+let i = 300;
+const intervalID = setInterval(function() {
+    grid = next();
+    draw();
+
+    console.log(i);
+    if (--i <= 0) {
+        clearInterval(intervalID);
+    }
+}, 100);
